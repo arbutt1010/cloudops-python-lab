0c7f855b738d0aa9b8087069a434bb4a629f2205
Abdul Rehman <arbutt1010@gmail.com>
2026-06-24 04:58:11 +0500

    week01 challenge

--- week01-python-basics/python-fundamentals/loops/week01_challenge.py
+++ week01-python-basics/python-fundamentals/loops/week01_challenge.py
@@ -74,23 +74,49 @@ orders = [
 #  Write your solution below this line.
 # ----------------------------------------------------------
 
-print(orders) # Our Dictionary name is orders, we can print it to see the data structure.
-#  ===== ORDER RECEIPTS =====
-#  Abdul : 2 x Latte      = $9.00
-#  Sara  : 1 x Espresso   = $3.00
-#  Ali   : 3 x Cappuccino = $12.00
-#  Bilal : 1 x Latte      = $4.50
-#  Amna  : 2 x Mocha      = $11.00
-#  Sara  : 1 x Mocha      = $5.50
-#  Ali   : 2 x Espresso   = $6.00
+# print(orders) # Our Dictionary name is orders, we can print it to see the data structure.
 print("===== ORDER RECEIPTS =====")
-# Code here 
+# Code here
+for order in orders:
+    cus = order["customer"]
+    qty = order["qty"]
+    drnk = order["drink"]
+    pric = order["price"] 
+    total_price = qty * pric
+    print(f"{cus} : {qty} x {drnk}  = ${total_price:.2f}")
 
 print("------ DAILY STATS ------")
-# Code here
+#  Total drinks sold : 12
+#  Total revenue     : $51.00
+#  Average order value: $7.29
+#  Biggest order     : Ali ($12.00)
+#  Orders over $5    : 4
 
-print("---- DRINKS SOLD BY TYPE ----")
-# Code here
+drinks_counter = 0
+revenue_counter = 0
+average_order_price = 0
+average_order_qty = 0
 
-print("---- LOYALTY ----")
-# Code here
+for order in orders:
+    # Total Drinks Sold
+    ttl_odr = order["qty"]
+    drinks_counter = drinks_counter + ttl_odr
+
+    # Total revenue
+    ttl_reve = order["price"] * order["qty"]
+    revenue_counter = revenue_counter + ttl_reve
+
+    # Average order
+    average_order = order["price"]
+    average_order_price = average_order_price + average_order
+    average_order_q = order["qty"]
+    average_order_qty = average_order_qty + average_order_q
+    average_order_real_price = average_order_price / average_order_qty
+
+    # Biggest order
+
+print(f"Total drinks sold : {drinks_counter}")
+print(f"Total revenue : ${revenue_counter: .2f}")
+print(f"Average order value : ${revenue_counter: .2f}")
+print(f"Average order value : ${average_order_real_price}")
+print(f"Biggest order : ${average_order_real_price}")
